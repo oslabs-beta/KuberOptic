8b59f10f36fbccef35745227adcf5cba85e54b94
Jimmy <jimmy@Jimmys-MacBook-Air.local>
2019-08-27 17:08:45 -0700

    working with new GCP cluster(s) one with 1 node and one with 3 node pools and 7 nodes

--- src/main/main.ts
+++ src/main/main.ts
@@ -3,16 +3,18 @@ const fetchGCP = require('./gcp/getGCPdata').default
 
 const GOOGLE_APPLICATION_CREDENTIALS:object = {
    "type": "service_account",
-   "project_id": "rich-ripple-250618",
-   "private_key_id": "593bb33b24f1f7c0aec67e3ade54945416bce88e",
-   "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC+ERA1lhvKzrpO\nleMj3ka3xGLl4Hi6yyw/QARyHS/FVBMn7KfbeNGuWJ7+Sll7VYj1bXfTfgpphSMm\n2LA1YGN/RWR0jGMMuucRb3LV4UUpqI+N+q/hUOUaTP2epri0gtCfp18HUUgLrkAV\nhTiWEWlUpwFtCVUz/9/63Ov6Q4E2K5qOtCa/Jq+zgegEXFJdkTfIgMNFnZH8J9HJ\nobD8Hg2LRsFoaRgPdlj+IzWdi1x7ie1k5/iqI3oOeOt02er/QUDUOgcvM540dEog\nh3V7tGGCWWnV3ktvusbtwh3DNMSgOQyhu0orTobJk9RUbEaVa5cDSKh2dnPx7fCT\nbR3XnJcvAgMBAAECggEACCk3ZvWhD948ha9N53Zd1vopvPEcffjHV5fqGPRD3tcz\nU12H/5Z6el/3w7dxjCgqD0+5+2zICavbzIk1V7Vh0E8sdwyoHB3SWh9M8P4ROC6y\nq0f+UVBl5fE0WqGSAL41MwW+wNOuokHDIqlrupXYzfe1yfbCn8720RC3WTGY1LiL\nlw1DJ0BQXc+IWULlaQHOrVvBVUuGb6iYR/TizxSs8UwGDIRS4lelMV2B3VTZsZZR\nJTDUrsi6gR46M8xO/Qk/1fJ5+gMjm2oZfdQzk9Wdf5ApWjwqruRFGngp+gaR6IMz\nqPueZdtSRgb+xqDc0O5YrX5Gp+pb6dOghEZpGbFYYQKBgQDsnRePPHmqAOKYoel6\nW2vq3ZQdWCwESyQpp1w3mpDHaNv0BwxpUJ0fARCEqPdX3fJNbLutK3F5vGGPQ+99\n9XPgsMdHRVbMRmNAPFRfR/wa+FhrWwNAIe2056SR9Hch4IHw2pFQ0NgBFx9SKDps\nGQL+sjZ6hrDEdxWWKDYZNqJoeQKBgQDNo6kgpu6O3++3YuGWsrS0KGIfjgZpMb0/\npVlrMTNjy1vCmKPg5wLAuDuzD/y5+jPZ35y/+e1hfpoPaC5nF/j2rpwPBCS5URKX\n/7OYeR1/vsyxHb4H7rzgph0kQyMrHGhPQJanKS5G/AdEWEJHfcImEXwPK4pi3PTj\nQI4LtyJi5wKBgF0ooJ0Bl3lKreygWveUxRGMugMvsuzh1NQdk55E2dnZPDiBCe7x\nEVuipKlGiVPsC7lWJTJSF3RxqgupGMMD/+KoV12FPF5jHDKvThzRj73ip5VlGQuI\nEDoYtd/NW5ZX+XEl7O7fuxv6yZckRZRH6Lj026Q0IabIqBQdsucMbrcpAoGAW+er\nLOauc7htwH426E0SKfpqcEPfZaBPPmS95ARnPVCjwALMKzXTqyIrvEmG3++MwPEJ\nc1tV1l7op74eDwFNyaIcKTHm39P2BWzIbXqjznYNsZLjXVQ7ogNbvtsqmpflnpX1\nPtxOzHbTIJ55cM4puS3TTkxdWvX5lJ9DXMUKXz0CgYB2LqRQYybnJx2RErqwvJch\nqYMcwTjzrZ1LZBdeDrsHSvILIPMRVNUGjWYSrydl9YIrH5ZlE0aGs4QweyGjk6nj\nm2J9C+LQvtifXj/UfvOz1rONGxmJgGCdJZ4EnSb+g2wiQvLx8ZbsgbrVw8089JTg\nOAJjMPGULs0bxL14CwGPzA==\n-----END PRIVATE KEY-----\n",
-   "client_email": "234608770302-compute@developer.gserviceaccount.com",
-   "client_id": "102504704420000835904",
+   "project_id": "kubernati",
+   "private_key_id": "d514feb8929365b6adaf6d6714676885522a062c",
+   "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCzA/Sb7Q//xCRz\nobJl2RyRQXwC5cRR8FDa8tudQOCGE8u5vikGBT28aNcTNpFSc2TaZMhxM47RxNkx\nyd9xUvnJf4XlxkYFyU1Wuimgu7IYk/bp0dioqGZ18CoKUC3hls9vKAJ4QjPAElyA\nxmY3hsxfsIoz74zFYScKUm1bLEo4AA9WgbihB3GE1Xk6o2HaW9ykDhQZ+UEsOZj3\na4RaaHerZ9hVyv735Ydxv4O8Uw7kfS0dr9AzWe4pLi6LXIrW1nrYSBXe2L7FwJbe\nOO+bmbUsyKS2fNIe9y+Cv/KRyzL/kKShvMmzPUQIhat0NETSZXCOUXJWMn4boTz9\nKr/85euhAgMBAAECggEAAM0ESrAI37nbuKN1HAroi38PsCbhXeU5hGA8oqeGFZ14\nLmmbDwCGIB4XM7odVHFJFsfHhIegtIApIVWUDQKMNOY/1elBGhCmapxYuDQMwfrD\n+pPucGM3+aA4XO1SxjkFrqtu9uUjbIEchjQ1A/uztXwhgyXXwzCKA1we6n7wSk+2\nVMCLr5ZaoMudpQ8c1ddqaDL/aPegNAImZnh/THgab/ARd6Zb5u8UDFvUlnavabqX\nTBi6k6QfcXWx11FrHMnRN29onDU9xJJSM/JbZVBTNxkxyxiWgCH8TNhZsZeZn/+4\nvAhH9iXvIglN6jw/oqb87OvTw/YdggVBmZEsvtSPgQKBgQDuA8va4p1mOtzjAteE\nLNihO6SI4yAdQ+QLs2TKh73bW+q1oGG56f+9RAxBaLiFv78LsfkWRcpr7NbpLnJW\nzwcq6vDR1Ke2VjAU6cDwNglm0t2FHr+CjfAgedrCHtzvTCIJfX5tt6xj7kVlpF4v\njHrUxIw12X9Ce7ruXik/zk0QwQKBgQDAit02atQp1AuFa7A8PNHDaV5exKas7mkr\nJeHtUG7FiBNm88aLJIH99+NwWO6FgfL9E0iOfhzjI5u3FML4v3QQBT5XT67YB5Si\nPxJbeawA7MwqhsMQ1WtQA8w3Nic2WnLuhmfy8Ah4qKGQRM5kYXofAWCo4553sxjK\nb9Vn8oSy4QKBgQC9xT8bRUXUBaiqQ1DMbOlIHILCNb0cOE4x2hXJuVLx/CZ2K+rf\nTaf/IqtW14UP9uL/EyaT0I9lX4+2mbbGagi/+lSKKLCTm8J4WivgGmWCmyvOAMcW\n8856Rk3aKrti/GPDB1Dvb9u+TXL9aIFEDhC9ZfyYztI9kuNnPKJM4lLlwQKBgC8e\nTIBSqFUMJT2jWt+C6rptgMCkUz2iom4CuUVAF7uTT1w0b/QBHSavkSMfQE+/u10f\naPQl/J/BSQwGsqf5AkghYF4xi9ImGMOTt8RXBDa9vEDz9aya7cYJB+LDB9mPTOz+\nS1XyU8BjVBZgIDoEeMQ5rBPokBEu7PQtZgYoHT1BAoGAae7BgjuPPr8gD0/S8J8F\nv9rAsNdKA+LWUnKkNx36CZbsTMNM48nFRkfSVd/PVWgauTUaD9xbn5bbEWT2qvjl\nkraIvC4XLpvksSkS23CQIxwNoZJAW915YsLux6l3PxKJgNodrtKLA6ahEh4GGJK0\ngDrUQIG1gMiZpaEV1QU12XE=\n-----END PRIVATE KEY-----\n",
+   "client_email": "458439475138-compute@developer.gserviceaccount.com",
+   "client_id": "105214681011184349548",
    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
    "token_uri": "https://oauth2.googleapis.com/token",
    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/234608770302-compute%40developer.gserviceaccount.com"
-}
+   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/458439475138-compute%40developer.gserviceaccount.com"
+ }
+ 
+ 
 
 async function getLocal() {
    const res = await fetchLocal();
