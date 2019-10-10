fc0a574617292e3697bef98211a06a1e0fe1316b
Darren Napier <dnaps5@gmail.com>
2019-10-10 10:32:38 -0700

    Up to date notes on topics/areas of study

---
+++ notes.js
@@ -0,0 +1,72 @@
+const due = {
+
+}
+
+const roles = () => {
+  let Dom = `AWS`
+  let Darren = `Websockets`
+  let Tim = `HotReload/Electron/Webpack`
+  let Bryan = `Docker`
+}
+
+const MVP = () => {
+
+  let awsCLI = `get it to work`;
+  let websocketsGCP = `incorporate`
+  let dockerImageUpload = `incorporate`
+  let OAuth = `OAuth of Kuber and GCP // Maybe google account login`
+  let hotReload = `Webpack and Electron`
+  let UI = `general improvement`
+
+}
+
+const stretchFeature = () => {
+
+  let performanceReport = `UI performance report of clusters`
+  let database = `incorporation of database`
+  // => MORE TO COME AS WE DIG MORE 
+
+}
+
+
+const frontend = () => {
+  let redux = `refactor codebase into redux`
+  let hooks = `incorporate hooks`
+  let typescript = `typescript refactoring`
+  let UI = {
+    navSidebar: `display side bar to left side of electron page`,
+    clusterPod: `display more info on each cluster`
+  }
+}
+
+const backend = () => {
+  let dockerImage = `ability to upload Docker img`
+  let AWS = `kubernetes AWS EKS` 
+  let database = {
+    stretchFeature: `performance report`
+  }
+  let websockets = `incorporate websockets for real time visual (Ref. KafkaLens)`
+}
+
+const unknown = () => {
+  let DomsQuestion = `What is the benifit of visualizing clusters?`
+  let miniCube = `practice local deploy of clusters`
+}
+
+// ***************************************************
+
+const loginCredentials = {
+  AWS_Google: {
+    User: `kuberoptic@gmail.com`,
+    Pass: `Kuber0ptic!` // 0 = Zero
+  }
+}
+
+// ***************************************************
+
+const links = {
+  AWS: `https://aws.amazon.com/kubernetes/`, // or https://aws.amazon.com/eks/
+  dockerGraphQL: `https://medium.com/maxime-heckel/building-a-graphql-wrapper-for-the-docker-api-2109f2b9c202`,
+  dockerDocs: `https://docs.docker.com/engine/api/v1.40/#`,
+  GoogleCloid: `https://console.cloud.google.com/home/dashboard?folder=&organizationId=&project=lexical-period-255501`
+}
