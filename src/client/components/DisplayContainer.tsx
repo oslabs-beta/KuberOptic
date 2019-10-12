3350d71ab0b89a7cab9f64f3a5759ade906f8f14
Timothy Mai <timothy.mai13@gmail.com>
2019-10-12 12:10:21 -0700

    fixed formatting and added comments describing functionality

--- src/client/components/DisplayContainer.tsx
+++ src/client/components/DisplayContainer.tsx
@@ -1,3 +1,14 @@
+/**
+ * ************************************
+ *
+ * @module  DisplayContainer.tsx
+ * @author
+ * @date
+ * @description container for the visualizer or (possibly) deployment settings (?)
+ *
+ * ************************************
+ */
+
 import * as React from 'react';
 import Deploy from './gcpDeploy';
 import { useContext } from 'react';
@@ -7,15 +18,17 @@ import Visualizer from './visualizer'
 const DisplayContainer = () => {
   const [Store, setStore] = useContext(StoreContext)
 
-    return (<div>{ Store.gcpDeployPage ? <Deploy/>:
+  // if gcpDeployPage is true, display Deploy
+  // else display Visualizer
+  return (
+    <div>
+      { Store.gcpDeployPage ? <Deploy/>:
         <div className='displayContainer'>
-        <Visualizer />
+          <Visualizer />
         </div>
-    }
+      }
     </div>
-    )
+  ) // maybe go back and remove these parentheses later; don't think they're necessary -Tim
 }
-
-
 
 export default DisplayContainer;
