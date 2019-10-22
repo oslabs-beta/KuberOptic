f2cb7520b4cdaa8f83720384023a3cd5a189a310
Timothy Mai <timothy.mai13@gmail.com>
2019-10-22 15:32:27 -0700

    removed some unnecessary comments

--- src/client/components/UploadPage.tsx
+++ src/client/components/UploadPage.tsx
@@ -34,58 +34,8 @@ const UploadPage = () => {
       clusters: null
     });
   };
-  
-/* MOVE THE FOLLOWING TO A SEPARATE COMPONENT
-
-  // function for handling location change
-  const handleLocation = (location: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
-    // updated to account for selecting multiple locations
-    const newGcpLoc = Store.gcploc;
-    newGcpLoc[location] = event.target.checked;
-
-    setStore({
-      ...Store, 
-      gcploc: newGcpLoc,
-    });
-  }
-
-  // making the options of locations to be displayed
-  const deployLocationStrings = [
-    'us-central1-a',
-    'us-central1-b',
-    'us-central1-c',
-    'southamerica-east1-a',
-    'southamerica-east1-b',
-    'southamerica-east1-c',
-    'europe-west2-a',
-    'us-west1-a'
-  ];
-
-  // deployLocations is what will be rendered later
-  // will be a bunch of checkboxes
-  const deployLocations = [];
-
-  deployLocationStrings.forEach(location => {
-    deployLocations.push(
-      <Checkbox 
-        checked={Store[location]}
-        handleChange={handleLocation}
-        value={location}
-        // maybe add a 'className' here, if needed
-        // original class name: .loc
-      />
-    );
-  });
-
-
-  <div id='uploadSelectMenu'>
-            {deployLocations}
-          </div>
-
-  */
 
   // functionalilty for pressing 'submit' button
-
   const handleSubmit = () => {
     const creds = JSON.parse(Store.credentials); 
     if (typeof creds !== 'object') {
